ed8219ef75eaac9a374264eb136403b9dc307570
Mitra Singh <mitra.coder@gmail.com>
2024-06-13 12:17:44 -0400

    add: new component to import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MyFirstCompComponent } from './my-first-comp/my-first-comp.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MyFirstCompComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-project';
