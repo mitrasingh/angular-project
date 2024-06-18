35ec2ebd03d095c4469aab0d3c287820073b745d
Mitra Singh <mitra.coder@gmail.com>
2024-06-17 22:25:18 -0400

    add: new component

---
+++ src/app/message-details/message-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './message-details.component.html',
+  styleUrl: './message-details.component.css'
+})
+export class MessageDetailsComponent {
+
+}
