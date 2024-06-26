49838f75b2d6fa3ec972f5c81349816a275ade63
Mitra Singh <mitra.coder@gmail.com>
2024-06-26 16:27:40 -0400

    update: create routes for app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MyFirstCompComponent } from './my-first-comp/my-first-comp.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: MyFirstCompComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+];
