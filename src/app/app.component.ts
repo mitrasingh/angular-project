b3a16211c9d1361ed34e3e6d9ac77b2169874d9d
Mitra Singh <mitra.coder@gmail.com>
2024-06-26 16:29:19 -0400

    update: add route imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { MyFirstCompComponent } from './my-first-comp/my-first-comp.component';
 import { MyFirstService } from './services/my-first.service';
 import { MenuComponent } from './menu/menu.component';
 import { AboutComponent } from './about/about.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MyFirstCompComponent, MenuComponent, AboutComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    MyFirstCompComponent,
+    MenuComponent,
+    AboutComponent,
+  ],
   providers: [MyFirstService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
