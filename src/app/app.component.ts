afda60b4a714ec58a0e75989a4ebbc21bf6471db
Mitra Singh <mitra.coder@gmail.com>
2024-06-25 20:35:09 -0400

    update: remove injector root object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MyFirstCompComponent } from './my-first-comp/my-first-comp.component';
+import { MyFirstService } from './services/my-first.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, MyFirstCompComponent],
+  providers: [MyFirstService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
