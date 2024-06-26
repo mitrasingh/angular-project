003626899b2c935a0aa9eccc448dc39eaa7d455a
Mitra Singh <mitra.coder@gmail.com>
2024-06-25 21:28:36 -0400

    update: add menu component to import array value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MyFirstCompComponent } from './my-first-comp/my-first-comp.component';
 import { MyFirstService } from './services/my-first.service';
+import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MyFirstCompComponent],
+  imports: [RouterOutlet, MyFirstCompComponent, MenuComponent],
   providers: [MyFirstService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
