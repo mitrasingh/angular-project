f1cdf4ffd9bf9d0f7a643e0e680647b2e0d9e1c1
Mitra Singh <mitra.coder@gmail.com>
2024-06-28 18:32:32 -0400

    update: remove import that are not needed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
 import { MyFirstCompComponent } from './my-first-comp/my-first-comp.component';
 import { MyFirstService } from './services/my-first.service';
 import { MenuComponent } from './menu/menu.component';
 import { AboutComponent } from './about/about.component';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    RouterLink,
-    MyFirstCompComponent,
-    MenuComponent,
-    AboutComponent,
-  ],
+  imports: [MyFirstCompComponent, MenuComponent, AboutComponent],
   providers: [MyFirstService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
